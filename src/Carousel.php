c10360a936da0ec1d2d9cdc4c70968b767b7f43f
DAUD ABASS MABENA <daudmabena1@gmail.com>
2020-12-07 16:00:44 +0300

    Update Carousel.php

--- src/Carousel.php
+++ src/Carousel.php
@@ -26,7 +26,7 @@ use yii\helpers\ArrayHelper;
  *         [
  *             'content' => '<img src="http://twitter.github.io/bootstrap/assets/img/bootstrap-mdo-sfmoma-03.jpg"/>',
  *             'caption' => '<h4>This is title</h4><p>This is the caption text</p>',
- *             'captionOptions' => ['class' => ['d-none', 'd-md-block']]
+ *             'captionOptions' => ['class' => ['d-none', 'd-md-block']],
  *             'options' => [...],
  *         ],
  *     ]
